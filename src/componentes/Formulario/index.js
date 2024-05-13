f3c0ce8c4514846821b26dbaf6f212c1f66bf169
jandirweb <jandirweb@gmail.com>
2024-05-13 16:11:23 -0300

    onsubmit do form

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -14,16 +14,23 @@ const Formulario = () => {
         'Mobile',
         'Inovação e Gestão'
     ]
+
+    const aoSalvar = (event) => {
+        event.preventDefault();
+        console.log("enviou o formulario!")
+    }
     
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome..." />
-                <CampoTexto label="Cargo" placeholder="Digite o seu cargo..." />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome..." />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo..." />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem..." />
-                <ListaSuspensa label="Time" itens={times}/>
-                <Botao>Criar card</Botao>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>
+                    Criar card
+                </Botao>
             </form>
         </section>
     )
