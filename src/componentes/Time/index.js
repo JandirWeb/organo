daa61aebd780b3cd0abec55925c80d50e2a36b1e
jandirweb <jandirweb@gmail.com>
2024-07-23 08:12:24 -0300

    ajuste para a mudanca de cor

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,16 +1,17 @@
 import Colaborador from '../Colaborador/Index'
 import './Time.css'
+import hexToRgba from 'hex-to-rgba';
 
 const Time = (props) => {
     return (
         props.colaboradores.length > 0 && 
-        <section className='time' style={{ backgroundColor: props.corSecundaria }}>
-            <input onChange={evento => props.mudarCor(evento.target.value, props.nome)} value={props.corSecundaria} className='input-cor' type='color'/>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        <section className='time' style={{ backgroundColor: hexToRgba(props.cor, '0.6') }}>
+            <input onChange={evento => props.mudarCor(evento.target.value, props.nome)} value={props.cor} className='input-cor' type='color'/>
+            <h3 style={{ borderColor: props.cor }}>{props.nome}</h3>
 
             <div className='colaboradores'>
                 {props.colaboradores.map( colaborador => {
-                    return <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} cor={props.corPrimaria} aoDeletar={props.aoDeletar}/>
+                    return <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} cor={props.cor} aoDeletar={props.aoDeletar}/>
                 })}
             </div>
         </section>
